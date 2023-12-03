9eb7b107a2f8378eccf369b107ca84d1c660613e
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2023-12-03 21:36:07 +0530

    stack intro

---
+++ intro.py
@@ -0,0 +1,10 @@
+#we can store elements by using array/list,LinkedList and another way of storing is using Stack
+#we cannnot remove inbetween the stack 
+#stack is about 3 things 
+#1.Push(insert in stack)
+#2.pop(delete in stack)
+#3.top(Access the top element of stack)
+
+
+#4.size(how many elements in stack)
+#5.isEmpty(Tells is stack empty or not gives boolean value(True)or False)
