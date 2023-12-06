62f928d7a0e3a51463735ab393a3b3cd8387c96c
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2023-12-06 07:50:47 +0530

    inbuild stack

--- stack&Queue_inbuilt.py
+++ stack&Queue_inbuilt.py
@@ -6,32 +6,50 @@
 
 import queue
 
-#inbuilt stack as a list
-s=[1,2,3]
-s.append(4)
-s.append(5)
+# #inbuilt stack as a list
+# s=[1,2,3]
+# s.append(4)
+# s.append(5)
 
-#This gives which element is getting poped
-print(s.pop())
-print(s.pop())
+# #This gives which element is getting poped
+# print(s.pop())
+# print(s.pop())
 
 
 #inbuild queue
 
 #we have made an object of that class(queue)
-q=queue.Queue()
-#we have funcitonlity like put get empty and etc
-#lets say i want to enque elements like queue
+# q=queue.Queue()
+# #we have funcitonlity like put get empty and etc
+# #lets say i want to enque elements like queue
+# q.put(1)
+# q.put(2)
+# q.put(3)
+# q.put(4)
+
+# #we have functionality called .empty
+# while not q.empty():
+#     #keep on removing elements
+#     #.get deques(remove) the element and it prints the element which was dequed
+#     print(q.get())
+
+
+#Queue follows FIFO property first in first out 
+
+
+
+#inbuild stack this queue has a class LIFO
+q=queue.LifoQueue()
 q.put(1)
 q.put(2)
 q.put(3)
 q.put(4)
 
-#we have functionality called .empty
+#this .empty is  an functionality
 while not q.empty():
-    #keep on removing elements
-    #.get deques(remove) the element and it prints the element which was dequed
+    #this will remove the last element
     print(q.get())
 
 
-#Queue follows FIFO property first in first out 
+
+    #Note:use this Queue library only for queue ,for stack use list that is better in interwiew
